86d2f1c49c204a966bc4bf980607f4543feab19f
msidolphin <OoMass-EffectoO@hotmail.com>
2019-05-22 13:05:39 +0800

    将路氹城划分至离岛

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import {Cascader, Select} from '../dist/index'
-// import {Cascader, Select} from './components'
+// import {Cascader, Select} from '../dist/index'
+import {Cascader, Select} from './components'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
